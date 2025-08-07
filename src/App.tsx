296a17e2985ebf77513ef3b6732e9c3da12b3449
moncef <meharzimoncef57@gmail.com>
2025-08-07 14:48:22 +0100

    add gutter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/navbar";
+
 function App() {
   return (
     <main className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
     </main>
   );
 }
